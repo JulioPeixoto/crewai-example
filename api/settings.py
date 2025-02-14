38e763fd081833c0bb5408f0228fba123dc4bd56
Julio Rafael <juliorafaelnft@hotmail.com>
2025-02-13 22:48:39 -0300

    Refactor index.html to use external CSS and update static file settings

--- api/settings.py
+++ api/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Configurações para arquivos de mídia
 MEDIA_URL = '/media/'
